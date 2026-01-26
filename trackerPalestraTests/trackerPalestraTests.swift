e6683909570cf50bc2bbdd6d896847cb991fa4b1
Tommus129 <tommasoprandini2001@gmail.com>
2026-01-26 22:40:10 +0100

    Initial Commit

---
+++ trackerPalestraTests/trackerPalestraTests.swift
@@ -0,0 +1,17 @@
+//
+//  trackerPalestraTests.swift
+//  trackerPalestraTests
+//
+//  Created by Tommaso Prandini on 26/01/26.
+//
+
+import Testing
+@testable import trackerPalestra
+
+struct trackerPalestraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
